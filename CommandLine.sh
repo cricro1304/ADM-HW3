261c5d3c3b1f0a1c338e360b9c8bb2c08b559917
Cristiano Perrone <80405122+cricro1304@users.noreply.github.com>
2023-11-25 14:08:32 +0100

    Create CommandLine.sh

---
+++ CommandLine.sh
@@ -0,0 +1,12 @@
+awk '(NR == 1) || (FNR > 1)' course_*.tsv > new_merged_file.tsv
+
+cut -f12 new_merged_file.tsv | sort | uniq -c | sort -nr
+
+cut -f11 new_merged_file.tsv | sort | uniq -c | sort -nr
+
+awk -F'\t' '$5=="False" {count++} END {print count}' new_merged_file.tsv
+
+total_courses=$(awk -F'\t' 'NR>1' new_merged_file.tsv | wc -l)
+engineering_courses=$(awk -F'\t' 'NR>1 && tolower($4) ~ /engineer/' new_merged_file.tsv | wc -l)
+percentage=$(engineering_courses * 100 / total_courses)
+echo $percentage
